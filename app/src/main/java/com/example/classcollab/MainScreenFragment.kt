027fa2fdb1526a64850061861376cd6803304d8c
cvam77 <schaudhary@patriots.uttyler.edu>
2021-06-05 21:51:57 -0500

    Created classes screen added

--- app/src/main/java/com/example/classcollab/MainScreenFragment.kt
+++ app/src/main/java/com/example/classcollab/MainScreenFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextClock
+import android.widget.TextView
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -31,6 +33,11 @@ class MainScreenFragment : Fragment() {
         createChannel.setOnClickListener(View.OnClickListener { view ->
             val action = MainScreenFragmentDirections.actionMainScreenFragmentToCreateClassChannelFragment(uid)
             Navigation.findNavController(view).navigate(action)
+        })
+
+        val createdClassesTV = view.findViewById<TextView>(R.id.created_classes_tv)
+        createdClassesTV.setOnClickListener(View.OnClickListener { view ->
+            Navigation.findNavController(view).navigate(R.id.action_mainScreenFragment_to_createdClasses)
         })
 //        val signOutBtn = view.
 //        // Inflate the layout for this fragment
