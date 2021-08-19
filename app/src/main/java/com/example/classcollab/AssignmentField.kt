68d54332bfaf5afc502e0813a74250172610831a
cvam77 <schaudhary@patriots.uttyler.edu>
2021-08-19 12:50:05 -0500

    deleted questionImageAdapter class and added to-dos

--- app/src/main/java/com/example/classcollab/AssignmentField.kt
+++ app/src/main/java/com/example/classcollab/AssignmentField.kt
@@ -1,42 +1,6 @@
 package com.example.classcollab
 
-import android.app.Activity.RESULT_OK
-import android.app.AlertDialog
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.provider.MediaStore
-import android.text.TextUtils
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.PopupMenu
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.classcollab.RecyclerView.LevelAdapter
-import com.example.classcollab.RecyclerView.QuestionImageAdapter
-import com.example.classcollab.databinding.FragmentAssignmentFieldBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.HashMap
 
 //You come to this Fragment if you go to an individual class and then click on ony option. For example, if you click on "assignments" or "tests" or
 //"book", you come to this class
---
+++ app/src/main/java/com/example/classcollab/notes.java
@@ -0,0 +1,16 @@
+package com.example.classcollab;
+
+import android.graphics.Bitmap;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
+
+public class notes {
+
+//    Next to-do:       1. showing comment time
+//                      2. Highlighting the first comment (actual question)
+
+
+}
